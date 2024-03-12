329fd5f6b4765e925782c484c8b4e3fee4849e90
azamovcoder <azamovcoder@gmail.com>
2024-03-12 23:37:12 +0300

    all

--- js/main.js
+++ js/main.js
@@ -1,14 +1,7 @@
 let menu = document.querySelector(".header__hamburger");
 let navBar =document.querySelector(".header__list");
-let dropdown = document.querySelector(".dropdown");
-let dropdownList = document.querySelector(".dropdown__list");
-let dropdownIcon = document.querySelector(".dropdown__icon");
-
-
-dropdown.addEventListener("click", function () {
-    dropdownList.classList.toggle
-        ("show__dropdown__list");
-    dropdownIcon.classList.toggle("show__dropdown__icon")
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel();
 });
 menu.addEventListener("click", function ()
 {
